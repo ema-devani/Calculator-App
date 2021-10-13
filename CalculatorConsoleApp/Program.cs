4fdd46c976571dcbce47efbe6677c64210ad2171
ema-devani <vaniputri0603@gmail.com>
2021-10-13 13:45:28 +0700

    Calculator Application

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorMachine;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("Inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine()); 
+
+            Console.Write("Inputkan nilai b = ");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            var cal = new Calculator();
+
+            Console.WriteLine(); 
+            Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, cal.Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, cal.Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian: {0} x {1} = {2}", a, b, cal.Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b));
+            Console.WriteLine("Hasil Pangkat: {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b));
+            Console.WriteLine("Hasil Module: {0} % {1} = {2}", a, b, Calculator.Modulo(a, b));
+
+            Console.ReadKey();
+        }
+
+        /*static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }*/
+    }
+}
---
+++ CalculatorWindowsFormsApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorMachine;
+
+namespace CalculatorWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        private int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        private int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        private int Pembagian(int a, int b)
+        {
+            return a / b;
+        }*/
+
+        private void btnHitung_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtNilaiA.Text);
+            var b = Convert.ToInt32(txtNilaiB.Text);
+
+            var cal = new Calculator();
+
+            lstHasil.Items.Clear();
+
+            lstHasil.Items.Add(string.Format("Hasil Penambahan: {0} + {1} = {2}", a, b, cal.Penambahan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pengurangan: {0} - {1} = {2}", a, b, cal.Pengurangan(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Perkalian: {0} x {1} = {2}", a, b, cal.Perkalian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pembagian: {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Pangkat: {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b)));
+            lstHasil.Items.Add(string.Format("Hasil Module: {0} % {1} = {2}", a, b, Calculator.Modulo(a, b)));
+
+        }
+    }
+}
